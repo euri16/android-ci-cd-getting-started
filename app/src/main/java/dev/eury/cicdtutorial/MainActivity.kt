4a1d5660151adc5a3c74486b4636e13259808616
Eury Perez <ebeltre@beachbody.com>
2021-02-14 14:44:15 -0400

    fixing Fastfile

--- app/src/main/java/dev/eury/cicdtutorial/MainActivity.kt
+++ app/src/main/java/dev/eury/cicdtutorial/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        Log.d("", "")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
